9c257fe789dc1d66e99be222a87d74dc5a050857
luanbrandao <luanbrandao4@gmail.com>
2022-06-05 00:32:23 -0300

    feat: add jest

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 Comands:
-npx tsc --init 
+npx tsc --init
 npm i rimraf
 
 // resolve o problme dos imports com @ quando gera o build
 npm i module-alias
 npm i @types/module-alias
 
-npm i -D @types/node    
+npm i -D @types/node
 
 npm i eslint-config-standard-with-typescript
- 
+
+npm i -D jest @types/jest ts-jest
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  collectCoverage: false,
+  coverageProvider: "babel",
+  moduleNameMapper: {
+    // config os @
+    "@/tests/(.+)": "<rootDir>/tests/$1", // o mais generico primeiro
+    "@/(.+)": "<rootDir>/src/$1",
+  },
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  transform: {
+    "\\.ts$": "ts-jest",
+  },
+};
