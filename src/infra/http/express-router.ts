4027745bc30a459fca471ff0660a88621293630c
luanbrandao <luanbrandao4@gmail.com>
2022-09-17 23:26:14 -0300

    transform ExpressRouter to a function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,11 +1,23 @@
 import { Controller } from '@/application/controllers'
-import { Request, Response } from 'express'
+import { Request, RequestHandler, Response } from 'express'
 
 export class ExpressRouter {
   constructor (private readonly controller: Controller) {}
 
   async adapt (req: Request, res: Response): Promise<void> {
     const httpResponse = await this.controller.handle({ ...req.body })
+
+    if (httpResponse.statusCode === 200) {
+      res.status(200).json(httpResponse.data)
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
+    }
+  }
+}
+
+export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+  return async (req, res) => {
+    const httpResponse = await controller.handle({ ...req.body })
 
     if (httpResponse.statusCode === 200) {
       res.status(200).json(httpResponse.data)
--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,14 +1,9 @@
-import { FacebookLoginController } from '@/application/controllers'
-import { FacebookAuthenticationService } from '@/data/services'
-import { FacebookApi } from '@/infra/apis'
-import { JwtTokenGenerator } from '@/infra/crypto'
-import { AxiosHttpClient } from '@/infra/http'
-import { PgUserAccountRepository } from '@/infra/postgres/repos'
 import { Router } from 'express'
-import { env } from '../config/env'
+import { makeFacebookLoginController } from '@/main/factories/controllers'
+import { adaptExpressRoute as adapt } from '@/infra/http'
 
 export default (router: Router): void => {
-  router.post('/api/login/facebook', (req, res) => {
-    res.send({ data: 'any_data' })
-  })
+  // const controller = makeFacebookLoginController()
+  // const adapter = new ExpressRouter(controller)
+  router.post('/api/login/facebook', adapt(makeFacebookLoginController()))
 }
