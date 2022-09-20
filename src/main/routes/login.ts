3ceccc88925a7e141979f22a2b8687751ae16bd2
luanbrandao <luanbrandao4@gmail.com>
2022-09-19 23:23:20 -0300

    add route prefix

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -5,5 +5,5 @@ import { adaptExpressRoute as adapt } from '@/infra/http'
 export default (router: Router): void => {
   // const controller = makeFacebookLoginController()
   // const adapter = new ExpressRouter(controller)
-  router.post('/api/login/facebook', adapt(makeFacebookLoginController()))
+  router.post('/login/facebook', adapt(makeFacebookLoginController()))
 }
