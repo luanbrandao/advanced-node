0c1d5602d56d79d18b3a615b253e3fa6e59b1b95
luanbrandao <luanbrandao4@gmail.com>
2022-09-20 23:37:26 -0300

    connect to postgres db before server init

---
+++ src/infra/postgres/helpers/config.ts
@@ -0,0 +1,11 @@
+import { ConnectionOptions } from 'typeorm'
+
+export const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5433,
+  username: 'postgres',
+  password: 'docker',
+  database: 'postgres',
+  entities: ['dist/infra/postgres/index.js']
+}
--- src/main/index.ts
+++ src/main/index.ts
@@ -4,5 +4,11 @@ import 'reflect-metadata' // reflect-metadata, para rodar os testes do pg
 
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+import { config } from '@/infra/postgres/helpers'
+import { createConnection } from 'typeorm'
 
-app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+createConnection(config)
+  .then(() =>
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  )
+  .catch(console.error)
