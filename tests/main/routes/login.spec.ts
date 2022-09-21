6bad6a106af7ff7fc41238e7a95f4b2a2bb3a955
luanbrandao <luanbrandao4@gmail.com>
2022-09-20 23:04:57 -0300

    add facebook login integration tests

--- tests/main/routes/login.spec.ts
+++ tests/main/routes/login.spec.ts
@@ -1,22 +1,22 @@
 import { app } from '@/main/config/app'
 import { PgUser } from '@/infra/postgres/entities'
 import { makeFakeDb } from '@/tests/infra/postgres/mocks'
+import { UnauthorizedError } from '@/application/errors'
 
 import { IBackup } from 'pg-mem'
 import { getConnection } from 'typeorm'
 import request from 'supertest'
 
-jest.mock('@/infra/apis/facebook', () => ({
-  FacebookApi: jest.fn().mockReturnValue({
-    loadUser: jest
-      .fn()
-      .mockResolvedValueOnce({ facebookId: 'any_id', name: 'any_name', email: 'any_email' })
-  })
-}))
-
 describe('Login Routes', () => {
   describe('Post /login/facebook', () => {
     let backup: IBackup
+
+    const loadUserSpy = jest.fn()
+    jest.mock('@/infra/apis/facebook', () => ({
+      FacebookApi: jest.fn().mockReturnValue({
+        loadUser: loadUserSpy
+      })
+    }))
 
     beforeAll(async () => {
       const db = await makeFakeDb([PgUser])
@@ -44,7 +44,27 @@ describe('Login Routes', () => {
       //   }
       // })
 
-      await request(app).post('/api/login/facebook').send({ token: 'valid_token' }).expect(200)
+      loadUserSpy.mockResolvedValueOnce({
+        facebookId: 'any_id',
+        name: 'any_name',
+        email: 'any_email'
+      })
+
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'valid_token' })
+
+      expect(status).toBe(200)
+      expect(body.accessToken).toBeDefined()
+    })
+
+    it('sould return 401 with UnauthorizedError', async () => {
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'invalid_token' })
+
+      expect(status).toBe(401)
+      expect(body.error).toBe(new UnauthorizedError().message)
     })
   })
 })
