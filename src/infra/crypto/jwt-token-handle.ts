151fd3d2d9daf8623599949b86ec366e83446f79
luanbrandao <luanbrandao4@gmail.com>
2022-09-28 22:24:13 -0300

    rename JwtTokenGenerator to JwtTokenHandler

--- src/infra/crypto/jwt-token-handle.ts
+++ src/infra/crypto/jwt-token-handle.ts
@@ -4,10 +4,10 @@ import jwt from 'jsonwebtoken'
 type Params = TokenGenerator.Params
 type Result = TokenGenerator.Result
 
-export class JwtTokenGenerator implements TokenGenerator {
-  constructor (private readonly secret: string) {}
+export class JwtTokenHandler implements TokenGenerator {
+  constructor(private readonly secret: string) {}
 
-  async generateToken ({ expirationInMs, key }: Params): Promise<Result> {
+  async generateToken({ expirationInMs, key }: Params): Promise<Result> {
     const expirationInSeconds = expirationInMs / 1000
     const token = jwt.sign({ key }, this.secret, { expiresIn: expirationInSeconds })
     return token
--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -1,12 +1,12 @@
 import { setupFacebookAuthentication, FacebookAuthentication } from '@/domain/use-cases'
 import { makeFacebookApi } from '@/main/factories/apis'
 import { makePgUserAccountRepo } from '@/main/factories/repos'
-import { makeJwtTokengenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
-    makeJwtTokengenerator()
+    makeJwtTokenHandler(),
   )
 }
