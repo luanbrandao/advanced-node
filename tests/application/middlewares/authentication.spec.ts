186e0ca06d64201ac5f8d89a2f0f341ededc177e
luanbrandao <luanbrandao4@gmail.com>
2022-09-29 23:48:38 -0300

    move AuthenticationMiddleware to its own file

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,29 +1,6 @@
-import { forbidden, HttpResponse, ok } from '@/application/helpers'
 import { ForbiddenError } from '@/application/errors/http'
-import { RequiredStringValidator } from '@/application/validation'
-import { Authorize } from '@/domain/use-cases'
+import { AuthenticationMiddleware } from '@/application/middlewares'
 
-type HttpRequest = { authorization: string }
-type Model = Error | { userId: string }
-
-class AuthenticationMiddleware {
-  constructor(private readonly authorize: Authorize) {}
-  async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    if (!this.validate({ authorization })) return forbidden()
-
-    try {
-      const userId = await this.authorize({ token: authorization })
-      return ok({ userId })
-    } catch (error) {
-      return forbidden()
-    }
-  }
-
-  private validate({ authorization }: HttpRequest): boolean {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    return error === undefined
-  }
-}
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
   let authorize: jest.Mock
